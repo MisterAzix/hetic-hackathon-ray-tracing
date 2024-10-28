083c8f9c901c187d3a0a595733a51a48633922d3
Charles Lambret <charleslambretpro@gmail.com>
2024-10-28 13:48:15 +0100

    toutes les operations setup

--- operations.cpp
+++ operations.cpp
@@ -6,7 +6,6 @@ private:
     float x, y, z;
 
 public:
-   
     Vecteur3D(float x, float y, float z) : x(x), y(y), z(z) {}
 
     Vecteur3D addition(const Vecteur3D& v) const {
@@ -25,5 +24,23 @@ public:
         return Vecteur3D(x * k, y * k, z * k);
     }
 
+    Vecteur3D normaliser() const {
+        float len = longueur();
+        if (len == 0) return Vecteur3D(0, 0, 0); 
+        return multiplicationParScalaire(1.0f / len);
+    }
+
+    float projectionSur(const Vecteur3D& a) const {
+        Vecteur3D a_normalized = a.normaliser();
+        return (x * a_normalized.x + y * a_normalized.y + z * a_normalized.z);
+    }
+
+    Vecteur3D projeterSur(const Vecteur3D& a) const {
+        float proj_length = projectionSur(a);
+        Vecteur3D a_normalized = a.normaliser();
+        return a_normalized.multiplicationParScalaire(proj_length);
+    }
 };
+
+
 

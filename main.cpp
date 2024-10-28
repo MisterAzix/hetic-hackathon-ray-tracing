9f2bb01a06c0d4680e5e65d4c409fe571f0a3896
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-10-28 14:44:05 +0100

    feat: generate gradient image ✨ (#4)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+constexpr int WINDOW_WIDTH = 800;
+constexpr int WINDOW_HEIGHT = 600;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "P3\n" << WINDOW_WIDTH << " " << WINDOW_HEIGHT << "\n255\n";
+    for (int j = WINDOW_HEIGHT - 1; j >= 0; j--) {
+        for (int i = 0; i < WINDOW_WIDTH; i++) {
+            float r = float(i) / float(WINDOW_WIDTH);
+            float g = float(j) / float(WINDOW_HEIGHT);
+            float b = 0.2;
+            int ir = static_cast<int>(255.99 * r);
+            int ig = static_cast<int>(255.99 * g);
+            int ib = static_cast<int>(255.99 * b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
 }

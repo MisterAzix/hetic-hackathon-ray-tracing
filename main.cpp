079cdb26bd1968209d985beb4b94b1f278f81e48
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-10-28 14:48:31 +0100

    feat: adding Color class ✨ (#5)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "src/Color.h"
 
 constexpr int WINDOW_WIDTH = 800;
 constexpr int WINDOW_HEIGHT = 600;
@@ -7,12 +8,10 @@ int main() {
     std::cout << "P3\n" << WINDOW_WIDTH << " " << WINDOW_HEIGHT << "\n255\n";
     for (int j = WINDOW_HEIGHT - 1; j >= 0; j--) {
         for (int i = 0; i < WINDOW_WIDTH; i++) {
-            float r = float(i) / float(WINDOW_WIDTH);
-            float g = float(j) / float(WINDOW_HEIGHT);
-            float b = 0.2;
-            int ir = static_cast<int>(255.99 * r);
-            int ig = static_cast<int>(255.99 * g);
-            int ib = static_cast<int>(255.99 * b);
+            Color color(static_cast<float>(i) / WINDOW_WIDTH, static_cast<float>(j) / WINDOW_HEIGHT, 0.2f);
+            int ir = static_cast<int>(255.99 * color.R());
+            int ig = static_cast<int>(255.99 * color.G());
+            int ib = static_cast<int>(255.99 * color.B());
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }

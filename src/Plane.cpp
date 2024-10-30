469e72755fbf8b363597719bf96a11b5264203e4
Benoît <61755435+benoitfvr@users.noreply.github.com>
2024-10-30 15:07:37 +0100

    Feat: add Checkerboard (#19)

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -3,11 +3,13 @@
 #include "Color.h"
 #include "Vector3.h"
 
-Plane::Plane(const Vector3 &point, const Vector3 &normal, const Color& color) : point(point), normal(normal.normalize()), color(color) {}
+Plane::Plane(const Vector3 &point, const Vector3 &normal, const Color &color) : point(point), normal(normal.normalize()), color(color) {}
 
-bool Plane::intersect(const Ray &ray, float &t) const {
+bool Plane::intersect(const Ray &ray, float &t) const
+{
     float denom = normal.dot(ray.direction);
-    if (std::abs(denom) > 1e-6) {
+    if (std::abs(denom) > 1e-6)
+    {
         Vector3 p0l0 = point - ray.origin;
         t = p0l0.dot(normal) / denom;
         return (t >= 0);
@@ -15,10 +17,24 @@ bool Plane::intersect(const Ray &ray, float &t) const {
     return false;
 }
 
-Color Plane::getColor() const {
+Color Plane::getColor() const
+{
     return color;
 }
 
-Vector3 Plane::getNormal(const Vector3 &point) const {
+Color Plane::getColorAt(const Vector3 &hit_point) const
+{
+    return getCheckerboardColor(hit_point);
+}
+
+Color Plane::getCheckerboardColor(const Vector3 &hit_point) const
+{
+    int checkerSize = 1;
+    bool isBlack = (static_cast<int>(std::floor(hit_point.x / checkerSize) + std::floor(hit_point.z / checkerSize)) % 2) == 0;
+    return isBlack ? Color(0.0f, 0.0f, 0.0f) : Color(1.0f, 1.0f, 1.0f);
+}
+
+Vector3 Plane::getNormal(const Vector3 &point) const
+{
     return normal;
 }
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -3,25 +3,34 @@
 #include "Color.h"
 #include <cmath>
 
-Sphere::Sphere(const Vector3 &center, const float radius, const Color& color) : center(center), radius(radius), color(color) {}
+Sphere::Sphere(const Vector3 &center, const float radius, const Color &color) : center(center), radius(radius), color(color) {}
 
-bool Sphere::intersect(const Ray &ray, float &t) const {
+bool Sphere::intersect(const Ray &ray, float &t) const
+{
     Vector3 vectorOC = ray.origin - center;
     float a = ray.direction.dot(ray.direction);
     float b = 2.0f * vectorOC.dot(ray.direction);
     float c = vectorOC.dot(vectorOC) - radius * radius;
     float discriminant = b * b - 4 * a * c;
-    if (discriminant < 0) {
+    if (discriminant < 0)
+    {
         return false;
     }
     t = (-b - std::sqrt(discriminant)) / (2.0f * a);
     return true;
 }
 
-Color Sphere::getColor() const {
+Color Sphere::getColor() const
+{
     return color;
 }
 
-Vector3 Sphere::getNormal(const Vector3 &point) const {
+Color Sphere::getColorAt(const Vector3 &) const
+{
+    return getColor();
+}
+
+Vector3 Sphere::getNormal(const Vector3 &point) const
+{
     return (point - center).normalize();
 }

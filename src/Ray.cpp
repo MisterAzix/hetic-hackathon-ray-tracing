1f71027e08dcfbd161053fe0440f1bd1122c5f53
Charles Lambret <charleslambretpro@gmail.com>
2024-10-30 11:05:23 +0100

    Implementation of ray reflection calculation

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -1,3 +1,10 @@
 #include "Ray.h"
 
 Ray::Ray(const Vector3 &origin, const Vector3 &direction) : origin(origin), direction(direction.normalize()) {}
+
+Ray Ray::reflect(const Vector3 &point, const Vector3 &normal) const
+{
+    Vector3 incident = direction;
+    Vector3 reflected = incident - 2 * (normal.dot(incident)) * normal;
+    return Ray(point, reflected.normalize());
+}
--- src/Ray.h
+++ src/Ray.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "Vector3.h"
 
+// Pour calculer le rayon réfléchi, renseigner le point d'impact et la normale de la surface 
 class Ray
 {
 public:
     Vector3 origin, direction;
 
     Ray(const Vector3 &origin, const Vector3 &direction);
+
+    Ray reflect(const Vector3 &point, const Vector3 &normal) const;
 };
